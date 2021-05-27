5e576ffce65279f6f4d1a7760a3fcf686e30f1d2
Mike Donnalley <mdonnalley@salesforce.com>
2021-05-27 14:23:37 -0600

    feat: add schemas:compare

--- src/commands/schemas/generate.ts
+++ src/commands/schemas/generate.ts
@@ -4,11 +4,76 @@ import {flags} from '@oclif/command'
 import {createGenerator, Schema} from 'ts-json-schema-generator'
 import {SnapshotCommand} from '../../snapshot-command'
 
+export class SchemaGenerator {
+  private classToId: Record<string, string> = {}
+
+  constructor(private base: SnapshotCommand) {}
+
+  public async generate(): Promise<Record<string, Schema>> {
+    for (const cmd of this.base.commands) {
+      // eslint-disable-next-line no-await-in-loop
+      const loadedCmd = await cmd.load() // commands are loaded async in oclif/core
+      this.classToId[loadedCmd.name] = loadedCmd.id
+    }
+
+    const schemas: Record<string, Schema> = {}
+
+    for (const file of this.getAllCmdFiles()) {
+      const {returnType, commandId} = this.parseFile(file)
+      const config = {
+        path: file,
+        type: returnType,
+        skipTypeCheck: true,
+      }
+      const schema = createGenerator(config).createSchema(config.type)
+      schemas[commandId] = schema
+    }
+
+    return schemas
+  }
+
+  private getAllCmdFiles(dirPath: string = path.join(this.base.config.root, 'src', 'commands'), allFiles: string[] = []) {
+    const files = fs.readdirSync(dirPath)
+
+    files.forEach(file => {
+      const fPath = path.join(dirPath, file)
+      if (fs.statSync(fPath).isDirectory()) {
+        allFiles = this.getAllCmdFiles(fPath, allFiles)
+      } else if (file.endsWith('ts')) {
+        allFiles.push(fPath)
+      }
+    })
+
+    return allFiles
+  }
+
+  private parseFile(file: string): { returnType: string; commandId: string } {
+    const returnTypeRegex = /(?<=async\srun\(\):\sPromise<)(.*?)(?=>)/g
+    const contents = fs.readFileSync(file, 'utf8')
+    const [returnType] = returnTypeRegex.exec(contents) as string[]
+    const commandId = this.determineCommandId(contents)
+    if (!commandId) {
+      throw new Error(`No commandId found for file ${file}`)
+    }
+
+    return {returnType, commandId}
+  }
+
+  private determineCommandId(contents: string): string | undefined {
+    for (const [className, cmdId] of Object.entries(this.classToId)) {
+      const regex = new RegExp(` class ${className} `, 'g')
+      if (regex.test(contents)) {
+        return cmdId
+      }
+    }
+  }
+}
+
 export default class SchemaGenerate extends SnapshotCommand {
     public static flags = {
-      directory: flags.string({
+      filepath: flags.string({
         description: 'directory to save the generated schema files; can use "{version}" to insert the current CLI/plugin version',
-        default: './schemas/{version}',
+        default: './schemas',
       }),
       singlefile: flags.boolean({
         description: 'put generated schema into a single file',
@@ -16,31 +81,13 @@ export default class SchemaGenerate extends SnapshotCommand {
       }),
     };
 
-    private classToId: Record<string, string> = {}
-
     public async run() {
       const {flags} = this.parse(SchemaGenerate)
+      const generator = new SchemaGenerator(this)
 
-      for (const cmd of this.commands) {
-        // eslint-disable-next-line no-await-in-loop
-        const loadedCmd = await cmd.load() // commands are loaded async in oclif/core
-        this.classToId[loadedCmd.name] = loadedCmd.id
-      }
+      const schemas = await generator.generate()
 
-      const schemas: Record<string, Schema> = {}
-
-      for (const file of this.getAllCmdFiles()) {
-        const {returnType, commandId} = this.parseFile(file)
-        const config = {
-          path: file,
-          type: returnType,
-          skipTypeCheck: true,
-        }
-        const schema = createGenerator(config).createSchema(config.type)
-        schemas[commandId] = schema
-      }
-
-      const directory = flags.directory.replace('{version}', this.config.version)
+      const directory = flags.filepath.replace('{version}', this.config.version)
       fs.mkdirSync(directory, {recursive: true})
 
       if (flags.singlefile) {
@@ -49,46 +96,9 @@ export default class SchemaGenerate extends SnapshotCommand {
         this.log(`Generated JSON schema file "${filePath}"`)
       } else {
         for (const [cmdId, schema] of Object.entries(schemas)) {
-          const fileName = cmdId.replace(/:/g, '-')
-          const filePath = path.join(directory, `${fileName}.json`)
+          const filePath = path.join(directory, `${cmdId}.json`)
           fs.writeFileSync(filePath, JSON.stringify(schema, null, 2))
           this.log(`Generated JSON schema file "${filePath}"`)
-        }
-      }
-    }
-
-    private getAllCmdFiles(dirPath: string = path.join(this.config.root, 'src', 'commands'), allFiles: string[] = []) {
-      const files = fs.readdirSync(dirPath)
-
-      files.forEach(file => {
-        const fPath = path.join(dirPath, file)
-        if (fs.statSync(fPath).isDirectory()) {
-          allFiles = this.getAllCmdFiles(fPath, allFiles)
-        } else if (file.endsWith('ts')) {
-          allFiles.push(fPath)
-        }
-      })
-
-      return allFiles
-    }
-
-    private parseFile(file: string): { returnType: string; commandId: string } {
-      const returnTypeRegex = /(?<=async\srun\(\):\sPromise<)(.*?)(?=>)/g
-      const contents = fs.readFileSync(file, 'utf8')
-      const [returnType] = returnTypeRegex.exec(contents) as string[]
-      const commandId = this.determineCommandId(contents)
-      if (!commandId) {
-        throw new Error(`No commandId found for file ${file}`)
-      }
-
-      return {returnType, commandId}
-    }
-
-    private determineCommandId(contents: string): string | undefined {
-      for (const [className, cmdId] of Object.entries(this.classToId)) {
-        const regex = new RegExp(` class ${className} `, 'g')
-        if (regex.test(contents)) {
-          return cmdId
         }
       }
     }
