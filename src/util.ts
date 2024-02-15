f3843fde073859b0bda9828f770ba32c467c0941
Mike Donnalley <mdonnalley@salesforce.com>
2024-02-15 11:18:33 -0700

    feat: support pattern strategy

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,6 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
 /**
  * Get the file name for a given command ID replacing "-" with "__" and ":" with "-"
  * @param cmdId - command ID
@@ -15,3 +18,26 @@ export const getSchemaFileName = (cmdId: string): string => {
  */
 export const getKeyNameFromFilename = (file: string): string =>
   file.replaceAll('-', ':').replaceAll('__', '-').replace('.json', '')
+
+export function getAllFiles(dirPath: string, ext: string, allFiles: string[] = []): string[] {
+  let files: string[] = []
+  try {
+    files = fs.readdirSync(dirPath)
+  } catch {}
+
+  for (const file of files) {
+    const fPath = path.join(dirPath, file)
+    if (fs.statSync(fPath).isDirectory()) {
+      allFiles = getAllFiles(fPath, ext, allFiles)
+    } else if (file.endsWith(ext)) {
+      allFiles.push(fPath)
+    }
+  }
+
+  return allFiles
+}
+
+export const GLOB_PATTERNS = [
+  '**/*.+(js|cjs|mjs|ts|tsx|mts|cts)',
+  '!**/*.+(d.ts|test.ts|test.js|spec.ts|spec.js|d.mts|d.cts)?(x)',
+]
