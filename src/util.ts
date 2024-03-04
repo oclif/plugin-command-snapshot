687e764a8dda7a4b984d8eec92910fd41734df57
Mike Donnalley <mdonnalley@salesforce.com>
2024-03-04 12:33:56 -0700

    feat: support pattern strategy (#482)
    * feat: support pattern strategy
    
    * chore: use dev tag for oclif/core
    
    * chore: code review
    
    * chore: bump core

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,6 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
 /**
  * Get the file name for a given command ID replacing "-" with "__" and ":" with "-"
  * @param cmdId - command ID
@@ -15,3 +18,24 @@ export const getSchemaFileName = (cmdId: string): string => {
  */
 export const getKeyNameFromFilename = (file: string): string =>
   file.replaceAll('-', ':').replaceAll('__', '-').replace('.json', '')
+
+export const getAllFiles = (dirPath: string, ext: string, allFiles: string[] = []): string[] =>
+  safeReadDirSync(dirPath)
+    .flatMap((f) =>
+      f.isDirectory() ? getAllFiles(path.join(dirPath, f.name), ext, allFiles) : path.join(dirPath, f.name),
+    )
+    .filter((f) => f.endsWith(ext))
+
+const safeReadDirSync = (dirPath: string): fs.Dirent[] => {
+  try {
+    // TODO: use recursive option when available in Node 20
+    return fs.readdirSync(dirPath, {withFileTypes: true})
+  } catch {
+    return []
+  }
+}
+
+export const GLOB_PATTERNS = [
+  '**/*.+(js|cjs|mjs|ts|tsx|mts|cts)',
+  '!**/*.+(d.ts|test.ts|test.js|spec.ts|spec.js|d.mts|d.cts)?(x)',
+]
