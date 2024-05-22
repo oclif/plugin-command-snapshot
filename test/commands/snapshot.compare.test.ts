8872808f031faef9eb2d5695f312e591d3361c1f
Mike Donnalley <mdonnalley@salesforce.com>
2024-05-22 13:58:55 -0600

    test: use oclif/test v4 (#558)
    * test: use oclif/test v4
    
    * chore: update dev deps

--- test/commands/snapshot.compare.test.ts
+++ test/commands/snapshot.compare.test.ts
@@ -1,29 +1,19 @@
-import {expect, test} from '@oclif/test'
-import sinon from 'sinon'
-
-const sandbox = sinon.createSandbox()
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
 
 describe('snapshot:compare', () => {
-  afterEach(() => {
-    sandbox.restore()
+  it('shows no changes', async () => {
+    const {stdout} = await runCommand('snapshot:compare')
+    expect(stdout).to.contain('No changes have been detected.')
   })
 
-  test
-    .stdout()
-    .command(['snapshot:compare'])
-    .it('shows no changes', (ctx) => {
-      expect(ctx.stdout).to.contain('No changes have been detected.')
-    })
-
-  test
-    .stdout()
-    .command(['snapshot:compare', '--filepath=./test/command-snapshot.json'])
-    .it('shows changes', (ctx) => {
-      expect(ctx.stdout).to.contain('The following commands and flags have modified')
-      expect(ctx.stdout).to.contain('\t-removed:command')
-      expect(ctx.stdout).to.contain('\t+snapshot:generate')
-      expect(ctx.stdout).to.contain('\t\t+filepath')
-      expect(ctx.stdout).to.contain('\t\t-all')
-      expect(ctx.stdout).to.contain('Since there are deletions, a major version bump is required.')
-    })
+  it('shows changes', async () => {
+    const {stdout} = await runCommand(['snapshot:compare', '--filepath=./test/command-snapshot.json'])
+    expect(stdout).to.contain('The following commands and flags have modified')
+    expect(stdout).to.contain('\t-removed:command')
+    expect(stdout).to.contain('\t+snapshot:generate')
+    expect(stdout).to.contain('\t\t+filepath')
+    expect(stdout).to.contain('\t\t-all')
+    expect(stdout).to.contain('Since there are deletions, a major version bump is required.')
+  })
 })
--- test/commands/snapshot.generate.test.ts
+++ test/commands/snapshot.generate.test.ts
@@ -1,26 +1,24 @@
-import {expect, test} from '@oclif/test'
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
 import sinon from 'sinon'
 
 import SnapshotCommand from '../../src/snapshot-command.js'
 
-const sandbox = sinon.createSandbox()
 let writeFileStub: sinon.SinonStub
 
 describe('snapshot:generate', () => {
   beforeEach(() => {
-    writeFileStub = sandbox.stub(SnapshotCommand.prototype, 'write')
+    writeFileStub = sinon.stub(SnapshotCommand.prototype, 'write')
   })
 
   afterEach(() => {
-    sandbox.restore()
+    sinon.restore()
   })
 
-  test
-    .stdout()
-    .command(['snapshot:generate'])
-    .it('runs command to generate snapshot file', (ctx) => {
-      expect(writeFileStub.calledOnce).to.be.true
-      expect(writeFileStub.getCall(0).firstArg).to.equal('./command-snapshot.json')
-      expect(ctx.stdout).to.contain('Generated snapshot file')
-    })
+  it('runs command to generate snapshot file', async () => {
+    const {stdout} = await runCommand('snapshot:generate')
+    expect(writeFileStub.calledOnce).to.be.true
+    expect(writeFileStub.getCall(0).firstArg).to.equal('./command-snapshot.json')
+    expect(stdout).to.contain('Generated snapshot file')
+  })
 })
