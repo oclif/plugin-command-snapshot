0f75a0ff430cb0fe482df117712c00f8a564e6a2
Mike Donnalley <mdonnalley@salesforce.com>
2024-05-22 09:43:51 -0600

    test: use oclif/test v4

--- test/commands/schema.compare.test.ts
+++ test/commands/schema.compare.test.ts
@@ -1,28 +1,17 @@
-import {expect, test} from '@oclif/test'
-import sinon from 'sinon'
-
-const sandbox = sinon.createSandbox()
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
 
 describe('schema:compare', () => {
-  afterEach(() => {
-    sandbox.restore()
+  it('shows no changes', async () => {
+    const {stdout} = await runCommand('schema:compare')
+    expect(stdout).to.contain('No changes have been detected.')
   })
 
-  test
-    .stdout()
-    .command(['schema:compare'])
-    .it('shows no changes', (ctx) => {
-      console.log(ctx.stdout)
-      expect(ctx.stdout).to.contain('No changes have been detected.')
-    })
-
-  test
-    .stdout()
-    .command(['schema:compare', '--filepath=./test/schemas'])
-    .it('shows changes', (ctx) => {
-      expect(ctx.stdout).to.contain('Found the following schema changes:')
-      expect(ctx.stdout).to.contain('- definitions.Snapshots.items.additionalProperties was added to latest schema')
-      expect(ctx.stdout).to.contain('- commands.snapshot:compare was not found in latest schema')
-      expect(ctx.stdout).to.contain('If intended, please update the schema file(s) and run again:')
-    })
+  it('shows changes', async () => {
+    const {stdout} = await runCommand(['schema:compare', '--filepath=./test/schemas'])
+    expect(stdout).to.contain('Found the following schema changes:')
+    expect(stdout).to.contain('- definitions.Snapshots.items.additionalProperties was added to latest schema')
+    expect(stdout).to.contain('- commands.snapshot:compare was not found in latest schema')
+    expect(stdout).to.contain('If intended, please update the schema file(s) and run again:')
+  })
 })
--- test/commands/schema.generate.test.ts
+++ test/commands/schema.generate.test.ts
@@ -1,30 +1,27 @@
-import {expect, test} from '@oclif/test'
-import * as path from 'node:path'
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
+import path from 'node:path'
 import sinon from 'sinon'
 
 import SnapshotCommand from '../../src/snapshot-command.js'
 
-const sandbox = sinon.createSandbox()
 let writeFileStub: sinon.SinonStub
 
 describe('schema:generate', () => {
   beforeEach(() => {
-    writeFileStub = sandbox.stub(SnapshotCommand.prototype, 'write')
+    writeFileStub = sinon.stub(SnapshotCommand.prototype, 'write')
   })
 
   afterEach(() => {
-    sandbox.restore()
+    sinon.restore()
   })
 
-  test
-    .stdout()
-    .command(['schema:generate'])
-    .it('runs command to generate schema files', (ctx) => {
-      // expect 4 calls because we have 4 command in this plugin
-      expect(writeFileStub.callCount).to.equal(4)
-      expect(ctx.stdout).to.contain(`Generated JSON schema file "${path.join('schemas/schema-compare.json')}"`)
-      expect(ctx.stdout).to.contain(`Generated JSON schema file "${path.join('schemas/schema-generate.json')}"`)
-      expect(ctx.stdout).to.contain(`Generated JSON schema file "${path.join('schemas/snapshot-compare.json')}"`)
-      expect(ctx.stdout).to.contain(`Generated JSON schema file "${path.join('schemas/snapshot-generate.json')}"`)
-    })
+  it('runs command to generate schema files', async () => {
+    const {stdout} = await runCommand('schema:generate')
+    expect(writeFileStub.callCount).to.equal(4)
+    expect(stdout).to.contain(`Generated JSON schema file "${path.join('schemas/schema-compare.json')}"`)
+    expect(stdout).to.contain(`Generated JSON schema file "${path.join('schemas/schema-generate.json')}"`)
+    expect(stdout).to.contain(`Generated JSON schema file "${path.join('schemas/snapshot-compare.json')}"`)
+    expect(stdout).to.contain(`Generated JSON schema file "${path.join('schemas/snapshot-generate.json')}"`)
+  })
 })
